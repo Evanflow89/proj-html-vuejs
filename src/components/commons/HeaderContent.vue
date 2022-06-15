f22961f1e736c5fca666764702f914d7b178c3cb
Evanflow89 <ivanbaragone@gmail.com>
2022-06-15 13:29:29 +0200

    added header content elements

--- src/components/commons/HeaderContent.vue
+++ src/components/commons/HeaderContent.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-6"></div>
+      <div class="col-6 d-flex flex-column justify-content-center">
+        <h1>Business Planning</h1>
+        <p class="thin">Digital Technology</p>
+        <p>Modern Solution !</p>
+      </div>
       <div class="col-6">
         <img
           class="img-fluid"
@@ -20,4 +24,17 @@ export default {
 </script>
 
 <style lang="scss">
+p {
+  color: var(--primary-color);
+  font-size: 3rem;
+  font-weight: 600;
+}
+
+.thin {
+  font-weight: 400;
+}
+
+.col-6 {
+  line-height: 3.125rem;
+}
 </style>
