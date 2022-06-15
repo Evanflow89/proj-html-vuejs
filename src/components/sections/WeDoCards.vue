cf94ee4845c8d900b8c5668141c32978b98a075f
Evanflow89 <ivanbaragone@gmail.com>
2022-06-15 16:53:29 +0200

    Change approach with cards

--- src/components/sections/WeDoCards.vue
+++ src/components/sections/WeDoCards.vue
@@ -1,41 +1,21 @@
 <template>
-  <div class="container"></div>
+  <section>
+    <div class="container">
+      <div class="row">
+        <ul>
+          <li class="col-lg-3"></li>
+          <li class="col-lg-3"></li>
+          <li class="col-lg-3"></li>
+          <li class="col-lg-3"></li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "WeDoCards",
-
-  data() {
-    return {
-      wedo: [
-        {
-          src: "../../style/images/Group-247.png",
-          title: "Data Analysis",
-          description:
-            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
-        },
-        {
-          src: "../../style/images/Group-567.png",
-          title: "SEO Optimization",
-          description:
-            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
-        },
-        {
-          src: "../../style/images/Group-538.png",
-          title: "Security Data",
-          description:
-            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
-        },
-        {
-          src: "../../style/images/Group-566.png",
-          title: "Branding Strategy",
-          description:
-            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
-        },
-      ],
-    };
-  },
 };
 </script>
 
