f727098e4db61ba3c8d981c76c412de83e90c4a5
Evanflow89 <ivanbaragone@gmail.com>
2022-06-15 16:21:09 +0200

    Added array with data

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -1,14 +1,16 @@
 <template>
   <main>
     <what-we-do />
+    <WeDoCards />
   </main>
 </template>
 
 <script>
 import WhatWeDo from "../sections/WhatWeDo.vue";
+import WeDoCards from "../sections/WeDoCards.vue";
 export default {
   name: "BaseMain",
-  components: { WhatWeDo },
+  components: { WhatWeDo, WeDoCards },
 };
 </script>
 
---
+++ src/components/sections/WeDoCards.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "WeDoCards",
+
+  data() {
+    return {
+      wedo: [
+        {
+          src: "../../style/images/Group-247.png",
+          title: "Data Analysis",
+          description:
+            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+        },
+        {
+          src: "../../style/images/Group-567.png",
+          title: "SEO Optimization",
+          description:
+            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+        },
+        {
+          src: "../../style/images/Group-538.png",
+          title: "Security Data",
+          description:
+            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+        },
+        {
+          src: "../../style/images/Group-566.png",
+          title: "Branding Strategy",
+          description:
+            "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
