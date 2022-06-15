d1b5daf8751789865174aeaede4a97e1e28f3ae9
Evanflow89 <ivanbaragone@gmail.com>
2022-06-15 16:59:11 +0200

    added cards content

--- src/components/sections/WeDoCards.vue
+++ src/components/sections/WeDoCards.vue
@@ -3,10 +3,50 @@
     <div class="container">
       <div class="row">
         <ul>
-          <li class="col-lg-3"></li>
-          <li class="col-lg-3"></li>
-          <li class="col-lg-3"></li>
-          <li class="col-lg-3"></li>
+          <li class="col-lg-3">
+            <img
+              src="../../assets/style/images/Group-247.png"
+              alt="Data Analysis"
+            />
+            <h4>Data Analysis</h4>
+            <p>
+              When, while the lovely valley teems with vapour around meand the
+              meridian sun strikes the upper
+            </p>
+          </li>
+          <li class="col-lg-3">
+            <img
+              src="../../assets/style/images/Group-567.png"
+              alt="Data Analysis"
+            />
+            <h4>SEO Optimization</h4>
+            <p>
+              When, while the lovely valley teems with vapour around meand the
+              meridian sun strikes the upper
+            </p>
+          </li>
+          <li class="col-lg-3">
+            <img
+              src="../../assets/style/images/Group-538.png"
+              alt="Security Data"
+            />
+            <h4>SEO Optimization</h4>
+            <p>
+              When, while the lovely valley teems with vapour around meand the
+              meridian sun strikes the upper
+            </p>
+          </li>
+          <li class="col-lg-3">
+            <img
+              src="../../assets/style/images/Group-566.png"
+              alt="Security Data"
+            />
+            <h4>Branding Strategy</h4>
+            <p>
+              When, while the lovely valley teems with vapour around meand the
+              meridian sun strikes the upper
+            </p>
+          </li>
         </ul>
       </div>
     </div>
