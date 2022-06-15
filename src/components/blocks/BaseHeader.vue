ca0ae4aca13232513be049e853cdfcdda10a97ee
Evanflow89 <ivanbaragone@gmail.com>
2022-06-15 12:07:24 +0200

    Added header background gradient

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+  name: "BaseHeader",
+};
+</script>
+
+<style lang="scss">
+header {
+  height: 75vh;
+  background-image: linear-gradient(#0b1953, #01012f);
+}
+</style>
