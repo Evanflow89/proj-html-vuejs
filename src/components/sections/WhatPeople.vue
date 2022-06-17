cd863c4c09a749e748ca4df1306d97c338b07826
Evanflow89 <ivanbaragone@gmail.com>
2022-06-17 09:25:41 +0200

    added fontawesome library

--- src/components/sections/WhatPeople.vue
+++ src/components/sections/WhatPeople.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="container what_people text-center">
-    <p class="p_head">Testimonial</p>
-    <h2 class="head"><span>What</span> People Say</h2>
-    <p class="p_content">
-      When, while the lovely valley teems with vapour around meand the meridian
-      sun strikes <br />
-      the upper surface.
-    </p>
-  </div>
+  <section>
+    <div class="container what_people text-center">
+      <p class="p_head">Testimonial</p>
+      <h2 class="head"><span>What</span> People Say</h2>
+      <p class="p_content">
+        When, while the lovely valley teems with vapour around meand the
+        meridian sun strikes <br />
+        the upper surface.
+      </p>
+    </div>
+    <div class="container people_cards">
+      <div class="row">
+        <div class="col-3"></div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -19,7 +26,7 @@ export default {
 <style lang="scss">
 .what_people {
   padding-top: 9.375rem;
-  padding-bottom: 6.25rem;
+  padding-bottom: 9.375rem;
   .p_head {
     color: var(--text-var1);
   }
