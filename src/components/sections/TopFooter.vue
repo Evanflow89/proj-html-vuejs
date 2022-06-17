bbea20e279687148b27d3b3dbc1224875e4b01ea
Evanflow89 <ivanbaragone@gmail.com>
2022-06-17 10:39:03 +0200

    Added top footer content

--- src/components/sections/TopFooter.vue
+++ src/components/sections/TopFooter.vue
@@ -14,9 +14,37 @@
             strikes the upper impenetrable
           </p>
         </div>
-        <div class="col-3"></div>
-        <div class="col-3"></div>
-        <div class="col-3"></div>
+        <div class="col-3">
+          <h5>Quick Links</h5>
+          <ul class="d-flex flex-column">
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h5>Resources</h5>
+          <ul class="d-flex flex-column align-items-end">
+            <li><a href="#">Art Design</a></li>
+            <li><a href="#">Computer</a></li>
+            <li><a href="#">IT News</a></li>
+            <li><a href="#">Networking</a></li>
+            <li><a href="#">Web Security</a></li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h5>Contact Us</h5>
+          <div class="address">
+            <p>
+              <span>Address:</span> Patricia C. Amedee 4401 Waldeck <br />
+              Street Grapevine Nashville, Tx 76051
+            </p>
+          </div>
+          <div class="contacts">
+            <p><span>Phone:</span> 099 (0) 101 0000 888</p>
+            <p><span>Email:</span> info@yourdomain.com</p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -30,6 +58,10 @@ export default {
 
 <style lang="scss">
 .top_footer {
+  ul {
+    padding: 0;
+    margin: 0;
+  }
   .logo img {
     width: 9.375rem;
   }
