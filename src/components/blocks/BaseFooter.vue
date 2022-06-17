f05f70436d174a9a649676fa20e43fd4415ce20c
Evanflow89 <ivanbaragone@gmail.com>
2022-06-17 10:26:06 +0200

    first col top footer

---
+++ src/components/blocks/BaseFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <top-footer />
+  </footer>
+</template>
+
+<script>
+import TopFooter from "../sections/TopFooter.vue";
+export default {
+  components: { TopFooter },
+  name: "BaseFooter",
+};
+</script>
+
+<style lang="scss">
+footer {
+  background-image: linear-gradient(#0b1953, #01012f);
+  padding-top: 12.5rem;
+  color: var(--text-gray);
+}
+</style>
---
+++ src/components/sections/TopFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row top_footer">
+        <div class="col-3">
+          <div class="logo pb-5">
+            <img
+              src="../../assets/style/images/white-logo-2.png"
+              alt="logo image"
+            />
+          </div>
+          <p>
+            When, while lovely valley teems vapour around meand meridian sun
+            strikes the upper impenetrable
+          </p>
+        </div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TopFooter",
+};
+</script>
+
+<style lang="scss">
+.top_footer {
+  .logo img {
+    width: 9.375rem;
+  }
+  p {
+    font-size: 0.875rem;
+  }
+}
+</style>
