5d5e058214fb0dcfad43b89ed299c46a561154ea
Evanflow89 <ivanbaragone@gmail.com>
2022-06-17 09:47:23 +0200

    added first card people

--- src/components/sections/WhatPeople.vue
+++ src/components/sections/WhatPeople.vue
@@ -9,9 +9,33 @@
         the upper surface.
       </p>
     </div>
+
+    <!-- ############# CARDS SECTION ################# -->
     <div class="container people_cards">
       <div class="row">
-        <div class="col-3"></div>
+        <div class="col-4">
+          <div class="img_people d-flex justify-content-center">
+            <img
+              src="../../assets/style/images/szabo-viktor-1266895-unsplash-1024x1024.png"
+              alt="User Image"
+              class="mb-5"
+            />
+          </div>
+          <div class="stars text-center pb-5">
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+            <i class="fa-regular fa-star"></i>
+          </div>
+          <p class="pb-3 text-center">
+            When, while lovely valley teems with vapour around meand meridian
+            sun strikes the upper impenetrable follage of my trees, and but a
+            thousand.
+          </p>
+          <h6 class="text-center">Michkel Anegl</h6>
+          <p class="text-center">Marketing Management</p>
+        </div>
       </div>
     </div>
   </section>
@@ -36,6 +60,23 @@ export default {
 
   .p_content {
     color: var(--text-black);
+  }
+}
+.people_cards {
+  .img_people img {
+    width: 7.5rem;
+    border: 4px solid #b6256f;
+    border-radius: 50%;
+  }
+  .fa-star {
+    color: #b6256f;
+  }
+  p {
+    font-size: 0.875rem;
+    font-weight: 500;
+  }
+  h6 {
+    font-weight: 600;
   }
 }
 </style>
